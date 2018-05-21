7c284f423b12cf325b7b8f73fe771ce7110d68cf
shawnwong65 <16022774@rp.edu.sg>
2018-05-21 10:43:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022774/demodatabasecrud/DBHelper.java
@@ -0,0 +1,77 @@
+package com.example.a16022774.demodatabasecrud;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "simplenotes.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NOTE = "note";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NOTE_CONTENT = "note_content";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_NOTE + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NOTE_CONTENT + " TEXT ) ";
+        db.execSQL(createNoteTableSql);
+
+        //Dummy Records
+        for (int i = 0; i < 4; i++){
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_NOTE_CONTENT, "Data number " + i);
+            db.insert(TABLE_NOTE, null, values);
+        }
+        Log.i("info", "Dummy records inserted.");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+        onCreate(db);
+    }
+
+    public long insertNote(String noteContent) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOTE_CONTENT, noteContent);
+        long result = db.insert(TABLE_NOTE, null, values);
+        db.close();
+        Log.d("SQL Insert ",""+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
+    public ArrayList<String> getAllNotes() {
+        ArrayList<String> notes = new ArrayList<String>();
+
+        String selectQuery = "SELECT " + COLUMN_ID + ","
+                + COLUMN_NOTE_CONTENT + " FROM " + TABLE_NOTE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String content = cursor.getString(1);
+                notes.add("ID:" + id + ", " + content);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return notes;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/a16022774/demodatabasecrud/Note.java
@@ -0,0 +1,29 @@
+package com.example.a16022774.demodatabasecrud;
+
+import java.io.Serializable;
+
+public class Note implements Serializable {
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+}
